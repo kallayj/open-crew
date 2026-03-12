e95ef5cd6b4db469f0b849af43b85d20a3c031b2
Jonathan Kallay <118477617+kallayj@users.noreply.github.com>
2026-03-12 18:06:42 +0000

    Implemented PWA

---
+++ src/service-worker.ts
@@ -0,0 +1,31 @@
+/// <reference types="@sveltejs/kit" />
+/// <reference lib="webworker" />
+
+import { build, files, prerendered, version } from '$service-worker';
+
+declare const self: ServiceWorkerGlobalScope;
+
+const CACHE = `app-${version}`;
+const ASSETS = [...build, ...files, ...prerendered];
+
+self.addEventListener('install', (event) => {
+	event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+	self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+	event.waitUntil(
+		caches
+			.keys()
+			.then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+	);
+	self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+	if (event.request.method !== 'GET') return;
+
+	event.respondWith(
+		caches.match(event.request).then((cached) => cached ?? fetch(event.request))
+	);
+});
